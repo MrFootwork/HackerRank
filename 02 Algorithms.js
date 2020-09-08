3be3ea8e73d087e6904a4bb82672a68916b50434
Mr Footwork <pandau.ting@gmail.com>
2020-09-08 15:42:59 +0200

    Daily Hacking

--- 02 Algorithms.js
+++ 02 Algorithms.js
@@ -62,3 +62,86 @@ function getTotalX(a, b) {
 }
 // console.log(getTotalX([ 2, 6 ], [ 24, 36 ]))
 // console.log(getTotalX([ 2, 4 ], [ 16, 32, 96 ]))
+
+/**Breaking the Records 
+ * @param {Number[]} scores seasonal scores
+ * @return {Number[]} [ high, low ]
+ * 
+ * high: amount of highscore breaks along the season
+ * 
+ * low: amount of lowscore breaks along the season
+*/
+function breakingRecords(scores) {
+	return scores
+		.reduce(
+			(records, score) => {
+				if (score > records[0][records[0].length - 1]) records[0].push(score)
+				if (score < records[1][records[1].length - 1]) records[1].push(score)
+				return records
+			},
+			[ [ scores[0] ], [ scores[0] ] ]
+		)
+		.map(record => record.length - 1)
+
+	let high = 0
+	let low = 0
+	let record = { high: scores[0], low: scores[0] }
+	for (let i in scores) {
+		if (scores[i] > record.high) {
+			record.high = scores[i]
+			high++
+		}
+		if (scores[i] < record.low) {
+			record.low = scores[i]
+			low++
+		}
+	}
+	return [ high, low ]
+}
+// console.log(breakingRecords([ 10, 5, 20, 20, 4, 5, 2, 25, 1 ]))
+// console.log(breakingRecords([ 3, 4, 21, 36, 10, 28, 35, 5, 24, 42 ]))
+
+/**Birthday Chocolate 
+ * @param {number[]} s numbers on each square of the chocolate bar
+ * @param {Number} d Ron's birthday
+ * @param {Number} m Ron's birth month
+ * @return {Number} total number of ways that Lily can portion the chocolate bar
+*/
+function birthday(s, d, m) {
+	let count = 0
+	for (let i = 0; i <= s.length - m; i++) {
+		if (s.slice(i, i + m).reduce((sum, el) => sum + el, 0) === d) count++
+	}
+	return count
+
+	//O(n) time, O(1) space
+	let counter = 0
+	let sum = 0
+	for (let i = 0; i < m; i++) sum += s[i]
+	if (sum === d) counter++
+	for (let i = m, j = 0; i < s.length; i++, j++) {
+		sum -= s[j]
+		sum += s[i]
+		if (sum === d) counter++
+	}
+	return counter
+}
+// console.log(birthday([ 1, 2, 1, 3, 2 ], 3, 2))
+// console.log(birthday([ 1, 1, 1, 1, 1, 1 ], 3, 2))
+
+/**Divisible Sum Pairs
+ * @param {Number} n array length of ar
+ * @param {Number[]} ar an array of numbers
+ * @param {Number} k number to divide the pair sum by
+ * @return {Number} number of pairs
+ */
+function divisibleSumPairs(n, k, ar) {
+	let pairs = []
+	for (let j = 1; j < n; j++) {
+		for (let i = 0; i < j; i++) {
+			if ((ar[i] + ar[j]) % k === 0) pairs.push([ i, j ])
+		}
+	}
+	return pairs.length
+}
+// console.log(divisibleSumPairs(6, 3, [ 1, 3, 2, 6, 1, 2 ]))
